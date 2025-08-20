43e0ca1cc2b8dbda1c90b407fadc383580dc9267
Ethan <ethanjohn.montejo@cit.edu>
2025-08-20 19:19:27 +0800

    innit commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,62 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+    public static double add(double x, double y) {
+        return x + y;
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+    public static double subtract(double x, double y) {
+        return x - y;
+    }
+
+    public static double multiply(double x, double y) {
+        return x * y;
+    }
+
+    public static double divide(double x, double y) {
+        if (y == 0) {
+            return Double.NaN;
         }
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to my calculator!");
+
+        System.out.print("Enter first integer: ");
+        double x = sc.nextFloat();
+
+        System.out.print("Enter an operation (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.print("Enter second integer: ");
+        double y = sc.nextFloat();
+
+        double res = 0;
+
+        switch (operator) {
+            case '+':
+                res = add(x, y);
+                break;
+            case '-':
+                res = subtract(x, y);
+                break;
+            case '*':
+                res = multiply(x, y);
+                break;
+            case '/':
+                res = divide(x, y);
+                if (Double.isNaN(res)) {
+                    System.out.println("Can't divide by 0.");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator select only this options (+, -, *, /)");
+        }
+
+        System.out.println(x + " " + operator + " " + y + " = " + res);
+
     }
 }
